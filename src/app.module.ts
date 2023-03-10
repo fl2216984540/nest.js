3f564a7809da7fecfac56b60ce499bdcd6fec579
fulai <fulai@xiongmaitech.com>
2023-03-10 19:28:47 +0800

    update

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,8 +11,8 @@ import "reflect-metadata";
     host: '127.0.0.1',
     port:3306,
     username: 'root',
-    password: 'root',
-    database:'nest-users'
+    password: '123456',
+    database:'nest_users'
   })],
   controllers: [AppController, UsersController],
   providers: [AppService],
